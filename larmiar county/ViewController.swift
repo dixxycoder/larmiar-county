3cb80955116b03913345a6436d7834c434043142
grant spraker <git@github.com:dixxycoder/iOSApps.git>
2022-07-29 15:22:07 -0600

    got it it have some of the look and the storyboard done.

--- larmiar county/ViewController.swift
+++ larmiar county/ViewController.swift
@@ -7,11 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+// Button Corner Radius and Border code from StackOverflow.com
+
+@IBDesignable extension UIButton{
+
+    @IBInspectable var borderWidth: CGFloat {
+
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
